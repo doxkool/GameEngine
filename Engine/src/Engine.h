f8afd07d2fbc3347c4a206138e63f57d4b9589e9
Dominic <Dominic@PROJECTX>
2023-12-30 21:10:45 -0500

    Forgot to update Engine.h

--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -2,6 +2,10 @@
 
 #include "Core/Application.h"
 #include "Core/Logger.h"
+#include "Core/Input.h"
 
 #include "Platforms/Windows/WinWindow.h"
 #include "Platforms/OpenGL/OpenGL_API.h"
+
+#include "Renderer/Shader.h"
+#include "Renderer/Camera.h"
